691365950ac80978fdcd78090afe17956c268172
Artur Duarte <arturduartemoraes@gmail.com>
2022-01-20 20:31:40 -0300

    :tada: First Commit

---
+++ app/src/main/java/com/example/libri/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.libri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.textSignIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        findViewById(R.id.imageBack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
